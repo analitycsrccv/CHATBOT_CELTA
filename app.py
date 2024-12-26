4e697de1264aa4a56e0b6ca94668e52783c6bae1
bescos15 <davidbescoscapdevila@hotmail.com>
2024-12-26 17:10:17 +0100

    Integración lenguaje natural SQL

--- app.py
+++ app.py
@@ -67,10 +67,10 @@ def agregar_mensajes_log(texto):
     nuevo_registro = Log(texto=texto)
     db.session.add(nuevo_registro)
     db.session.commit()
-
+    
 TOKEN_ANDERCODE = "ANDERCODE"
 
-@app.route('/webhook', methods=['GET', 'POST'])
+@app.route('/webhook', methods=['GET','POST'])
 def webhook():
     if request.method == 'GET':
         return verificar_token(request)
@@ -81,7 +81,7 @@ def verificar_token(req):
     challenge = req.args.get('hub.challenge')
     if challenge and token == TOKEN_ANDERCODE:
         return challenge
-    return jsonify({'error': 'Token Invalido'}), 401
+    return jsonify({'error':'Token Invalido'}), 401
 
 def recibir_mensajes(req):
     try:
@@ -121,56 +121,289 @@ def enviar_mensajes_whatsapp(texto, number):
     texto = texto.lower()
     palabras_consulta = ['mostrar', 'ver', 'buscar', 'muestra', 'dime', 'agregar', 'añadir', 'crear']
     
-    try:
-        if any(palabra in texto for palabra in palabras_consulta):
-            response = natural_to_sql(texto)
-            data = {
-                "messaging_product": "whatsapp",
-                "to": number,
-                "type": "text",
-                "text": {
-                    "preview_url": False,
-                    "body": response
-                }
+    if any(palabra in texto for palabra in palabras_consulta):
+        response = natural_to_sql(texto)
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": response
             }
-        elif "hola" in texto:
-            data = {
-                "messaging_product": "whatsapp",
-                "to": number,
-                "type": "text",
-                "text": {
-                    "preview_url": False,
-                    "body": "🚀 ¡Hola! ¿Cómo estás? Bienvenido.\n\nPuedes usar comandos como:\n- Ver mensajes\n- Agregar mensaje\n- Mostrar registros"
-                }
-            }
-        else:
-            respuesta = get_chatgpt_response(texto)
-            data = {
-                "messaging_product": "whatsapp",
-                "to": number,
-                "type": "text",
-                "text": {
-                    "preview_url": False,
-                    "body": respuesta
-                }
-            }
-
-        data = json.dumps(data)
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer EAAI8epaNZBKABO2y05KV992fooZCdvZBMwaF0qvCFSQ5IhGEKyoSAXavSIefZBRcVaDQ8YWBAShiWqZBTZCJOjjqe02B8JZASfJNBPFrUzZAD7FSkjOfxWS9Ii2tsDBmp66vF4ZCgc7ip6xvzEiFe38bYcZCmdeUZARfCJ0JRFewaQAW2ZAnidhJbijX48BXOji155xwiLB1YeF5Fcdz8Tvym5jZCfYr8FDimW8foni4ZD"
         }
-        
-        connection = http.client.HTTPSConnection("graph.facebook.com")
+    elif "hola" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "🚀 ¡Hola! ¿Cómo estás? Bienvenido.\n\nPuedes usar comandos como:\n- Ver mensajes\n- Agregar mensaje\n- Mostrar registros"
+            }
+        }
+    elif "1" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            }
+        }
+    elif "2" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "to": number,
+            "type": "location",
+            "location": {
+                "latitude": "-12.067158831865067",
+                "longitude": "-77.03377940839486",
+                "name": "Estadio Nacional del Perú",
+                "address": "Cercado de Lima"
+            }
+        }
+    elif "3" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "document",
+            "document": {
+                "link": "https://www.turnerlibros.com/wp-content/uploads/2021/02/ejemplo.pdf",
+                "caption": "Temario del Curso #001"
+            }
+        }
+    elif "4" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "audio",
+            "audio": {
+                "link": "https://filesamples.com/samples/audio/mp3/sample1.mp3"
+            }
+        }
+    elif "5" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "to": number,
+            "text": {
+                "preview_url": True,
+                "body": "Introduccion al curso! https://youtu.be/6ULOE2tGlBM"
+            }
+        }
+    elif "6" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "🤝 En breve me pondre en contacto contigo. 🤓"
+            }
+        }
+    elif "7" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "📅 Horario de Atención : Lunes a Viernes. \n🕜 Horario : 9:00 am a 5:00 pm 🤓"
+            }
+        }
+    elif "0" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "🚀 Hola, visita mi web anderson-bastidas.com para más información.\n \n📌Por favor, ingresa un número #️⃣ para recibir información.\n \n1️⃣. Información del Curso. ❔\n2️⃣. Ubicación del local. 📍\n3️⃣. Enviar temario en PDF. 📄\n4️⃣. Audio explicando curso. 🎧\n5️⃣. Video de Introducción. ⏯️\n6️⃣. Hablar con AnderCode. 🙋‍♂️\n7️⃣. Horario de Atención. 🕜 \n0️⃣. Regresar al Menú. 🕜"
+            }
+        }
+    elif "boton" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "interactive",
+            "interactive": {
+                "type": "button",
+                "body": {
+                    "text": "¿Confirmas tu registro?"
+                },
+                "footer": {
+                    "text": "Selecciona una de las opciones"
+                },
+                "action": {
+                    "buttons": [
+                        {
+                            "type": "reply",
+                            "reply": {
+                                "id": "btnsi",
+                                "title": "Si"
+                            }
+                        },
+                        {
+                            "type": "reply",
+                            "reply": {
+                                "id": "btnno",
+                                "title": "No"
+                            }
+                        },
+                        {
+                            "type": "reply",
+                            "reply": {
+                                "id": "btntalvez",
+                                "title": "Tal Vez"
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+    elif "btnsi" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Muchas Gracias por Aceptar."
+            }
+        }
+    elif "btnno" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Es una Lastima."
+            }
+        }
+    elif "btntalvez" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Estare a la espera."
+            }
+        }
+    elif "lista" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "to": number,
+            "type": "interactive",
+            "interactive": {
+                "type": "list",
+                "body": {
+                    "text": "Selecciona Alguna Opción"
+                },
+                "footer": {
+                    "text": "Selecciona una de las opciones para poder ayudarte"
+                },
+                "action": {
+                    "button": "Ver Opciones",
+                    "sections": [
+                        {
+                            "title": "Compra y Venta",
+                            "rows": [
+                                {
+                                    "id": "btncompra",
+                                    "title": "Comprar",
+                                    "description": "Compra los mejores articulos de tecnologia"
+                                },
+                                {
+                                    "id": "btnvender",
+                                    "title": "Vender",
+                                    "description": "Vende lo que ya no estes usando"
+                                }
+                            ]
+                        },
+                        {
+                            "title": "Distribución y Entrega",
+                            "rows": [
+                                {
+                                    "id": "btndireccion",
+                                    "title": "Local",
+                                    "description": "Puedes visitar nuestro local."
+                                },
+                                {
+                                    "id": "btnentrega",
+                                    "title": "Entrega",
+                                    "description": "La entrega se realiza todos los dias."
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    elif "btncompra" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Los mejos articulos top en ofertas."
+            }
+        }
+    elif "btnvender" in texto:
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Excelente elección."
+            }
+        }
+    else:
+        respuesta = get_chatgpt_response(texto)
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": respuesta
+            }
+        }
+
+    data = json.dumps(data)
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer EAAI8epaNZBKABO2y05KV992fooZCdvZBMwaF0qvCFSQ5IhGEKyoSAXavSIefZBRcVaDQ8YWBAShiWqZBTZCJOjjqe02B8JZASfJNBPFrUzZAD7FSkjOfxWS9Ii2tsDBmp66vF4ZCgc7ip6xvzEiFe38bYcZCmdeUZARfCJ0JRFewaQAW2ZAnidhJbijX48BXOji155xwiLB1YeF5Fcdz8Tvym5jZCfYr8FDimW8foni4ZD"
+    }
+    
+    connection = http.client.HTTPSConnection("graph.facebook.com")
+    try:
         connection.request("POST", "/v21.0/484107578125461/messages", data, headers)
         response = connection.getresponse()
-        print(f"Status: {response.status}, Reason: {response.reason}")
-        
+        print(response.status, response.reason)
     except Exception as e:
-        print(f"Error en enviar_mensajes_whatsapp: {str(e)}")
+        agregar_mensajes_log(json.dumps(str(e)))
     finally:
-        if 'connection' in locals():
-            connection.close()
+        connection.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)   
+    app.run(host='0.0.0.0', port=80, debug=True)     
